cb72885ebbe30a90015680e76957f2431694a03c
EmillyDelf <emillyvitoriaprog@gmail.com>
2026-03-11 00:51:07 -0300

    adicionando o teste 8 do vetor

--- .idea/src/Atividade_D/TesteVetor8.java
+++ .idea/src/Atividade_D/TesteVetor8.java
@@ -1,4 +1,28 @@
-package Atividade_D;
+package vetor;
 
 public class TesteVetor8 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("A");
+        vetor.adiciona("B");
+        vetor.adiciona("C");
+        vetor.adiciona("B");
+        vetor.adiciona("D");
+
+        System.out.println("Vetor inicial: " + vetor);
+
+        System.out.println("\nRemovendo B");
+        boolean removido = vetor.remove("B");
+        System.out.println("Removido: " + removido);
+        System.out.println(vetor);
+
+        System.out.println("\nRemovendo X");
+        boolean removido2 = vetor.remove("X");
+        System.out.println("Removido: " + removido2);
+        System.out.println(vetor);
+
+    }
 }

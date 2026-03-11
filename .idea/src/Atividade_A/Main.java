ec7b78d00fee70a36fabf59a12808cd047174188
Luiz-Otavio15 <oluiz9790@gmail.com>
2026-03-11 10:45:43 -0300

    Ajustando main

--- .idea/src/Atividade_A/Main.java
+++ .idea/src/Atividade_A/Main.java
@@ -11,6 +11,8 @@ public class Main {
         System.out.println("Executando TesteVetorB2...");
         TesteVetorB2.main(args);
 
+        System.out.println("\n---------------------------\n");
+
         System.out.println("Executando TesteVetorB3...");
         TesteVetorB3.main(args);
 
@@ -18,6 +20,8 @@ public class Main {
 
         System.out.println("Executando TesteVetorC4...");
         TesteVetorC4.main(args);
+
+        System.out.println("\n---------------------------\n");
 
         System.out.println("Executando TesteVetorC5...");
         TesteVetorC5.main(args);
@@ -27,5 +31,6 @@ public class Main {
         System.out.println("Executando TesteVetorC6...");
         TesteVetorC6.main(args);
 
+        System.out.println("\n---------------------------\n");
     }
 }

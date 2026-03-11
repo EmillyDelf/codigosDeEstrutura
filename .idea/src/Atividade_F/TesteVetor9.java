038058a971f8559e17b83849ec25ac6928789527
Adão Vitor <super42066@gmail.com>
2026-03-11 01:39:15 -0300

    PARTE 13 E 14

---
+++ .idea/src/Atividade_F/TesteVetor9.java
@@ -0,0 +1,26 @@
+package Atividade_F;
+
+
+public class TesteVetor9 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("A");
+        vetor.adiciona("B");
+        vetor.adiciona("C");
+        vetor.adiciona("B");
+        vetor.adiciona("D");
+        vetor.adiciona("B");
+
+        System.out.println("Vetor: " + vetor);
+
+        int indice = vetor.indiceUltimo("B");
+
+        System.out.println("Última ocorrência de B: " + indice);
+
+        int indice2 = vetor.indiceUltimo("X");
+
+        System.out.println("Última ocorrência de X: " + indice2);
+    }
+}
---
+++ .idea/src/Atividade_F/Testevetor10.java
@@ -0,0 +1,24 @@
+package Atividade_F;
+
+public class Testevetor10 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("A");
+        vetor.adiciona("B");
+        vetor.adiciona("C");
+        vetor.adiciona("B");
+        vetor.adiciona("D");
+        vetor.adiciona("B");
+
+        System.out.println("Vetor inicial: " + vetor);
+
+        System.out.println("\nRemovendo todos os B");
+
+        vetor.removerTodos("B");
+
+        System.out.println("Vetor final: " + vetor);
+    }
+}

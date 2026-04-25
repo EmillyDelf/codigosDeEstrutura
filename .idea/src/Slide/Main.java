168f5b7e5bb42f98502456bb2cbbe245ae7304d9
Luiz-Otavio15 <oluiz9790@gmail.com>
2026-04-24 22:26:31 -0300

    Selection

---
+++ .idea/src/Slide/Main.java
@@ -0,0 +1,15 @@
+package Slide;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = {5, 3, 8, 4, 2};
+
+        Selection selection =  new Selection();
+        selection.selectionSort(numeros);
+
+        for (int num : numeros){
+            System.out.println(num + " ");
+        }
+    }
+}
---
+++ .idea/src/Slide/Selection.java
@@ -0,0 +1,26 @@
+package Slide;
+
+public class Selection {
+
+
+    public void selectionSort(int []vetor){
+
+
+        for (int i = 0; i < vetor.length - 1; i++) {
+            int menorIndex = i;
+
+            for (int j = i + 1 ; j < vetor.length; j++) {
+                if (vetor[j] < vetor[menorIndex]) {
+                    menorIndex = j;
+                }
+            }
+
+            int temp = vetor[i];
+            vetor[i] = vetor[menorIndex];
+            vetor[menorIndex] = temp;
+
+        }
+    }
+
+
+}
